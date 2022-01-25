3fecb3e51fe1bb5e341b7eabddc566aa5fec96f4
Gruwpy <75033204+Gruwpy@users.noreply.github.com>
2022-01-25 17:46:59 -0500

    Create java.js

---
+++ java.js
@@ -0,0 +1,3 @@
+<div id="sfckwb65hrhlyncnfl6xp3tew377tbcsxp9"></div>
+<script type="text/javascript" src="https://counter2.stat.ovh/private/counter.js?c=kwb65hrhlyncnfl6xp3tew377tbcsxp9&down=async" async></script>
+<br><a href="https://www.freecounterstat.com">free website counter</a><noscript><a href="https://www.freecounterstat.com" title="free website counter"><img src="https://counter2.stat.ovh/private/freecounterstat.php?c=kwb65hrhlyncnfl6xp3tew377tbcsxp9" border="0" title="free website counter" alt="free website counter"></a></noscript>
